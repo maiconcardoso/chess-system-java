983479d2c883725e089f2ad8efd977fa7fb25abb
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-06-25 06:31:02 -0300

    Handling exceptions and clearing screen

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import boardgame.BoardException;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -12,16 +15,30 @@ public class App {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
-            
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+            try {
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+                
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+    
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                
+            } catch (ChessException e) {
+                System.out.print(e.getMessage());
+                sc.nextLine();
+            } catch (BoardException e) {
+                System.out.print(e.getMessage());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.print(e.getMessage());
+                sc.nextLine();
+            } 
         }
     }
 }
